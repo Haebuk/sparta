154e8d6d71415a8d2b9803fff5973af56647ac02
Jaesung Ryu <cwryu6252@naver.com>
2021-08-28 17:22:52 +0900

    week 4,5 done

--- sparta_nodejs/routers/goods.js
+++ sparta_nodejs/routers/goods.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const Goods = require("../schemas/Goods");
-
+const Goods = require("../schemas/goods");
+const Cart = require("../schemas/cart");
 const router = express.Router();
 
 router.get("/goods", async (req, res, next) => {
@@ -29,5 +29,19 @@ router.post('/goods', async (req, res) => {
     }
     res.send({ result: "success" });
   });
+
+router.post("/goods/:goodsId/cart", async (req, res) => {
+  const { goodsId } = req.params;
+  const { quantity } = req.body;
+
+  isCart = await Cart.find({ goodsId });
+  console.log(isCart, quantity);
+  if (isCart.length) {
+    await Cart.updateOne({ goodsId }, { $set: { quantity } });
+  } else {
+    await Cart.create({ goodsId: goodsId, quantity: quantity });
+  }
+  res.send({ result: "success" });
+});
 
 module.exports = router;
